c0711a518efbbace5fcc273d09c4e85a195c3a8f
Tinnitus <qiusheng95@gmail.com>
2018-05-06 17:28:10 +0200

    added weather

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -3,12 +3,29 @@ import SideNavHeader from './SideNavHeader';
 
 import SideNavFooter from './SideNavFooter';
 import SideNavOptions from './SideNavOptions';
+
+const API_KEY = 'e8fed75c765bc183cc0bf22d745d8c10';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/weather?q=Stockholm,swe&appid=${API_KEY}`;
+
+
 class SideNav extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            weather: []
+        }
+    }
+    componentDidMount(){
+        fetch(ROOT_URL)
+        .then(response => response.json())
+        .then(data => this.setState({ weather: data.main.temp }));
+    }
     render(){
+        console.log(this.state.weather);
         return (
             <nav className="sidebar">
                 <div className="sidebarContainer">
-                    <SideNavHeader/>
+                    <SideNavHeader weather={this.state.weather}/>
                     <SideNavOptions/>
                     <SideNavFooter/>
                 </div>
--- src/components/SideNavHeader.js
+++ src/components/SideNavHeader.js
@@ -1,16 +1,26 @@
 import React from 'react';
 
 
- const SideNavHeader = () => {
+const SideNavHeader = (props) => {
+    console.log(props);
+    let currentTemp = props.weather ?
+    (props.weather - 273.15).toFixed(1)
+    :
+        'Fetching weather...';
+
     return (
         <div className="header">
             <h1>Oscar Fredriksson</h1>
             <h3>Front End Developer</h3>
-             <p>Student at KYH in Stockholm, Sweden. <br/>
+            <p>Student at KYH in Stockholm, Sweden. <br/>
             I'm a passionate Web Developer, with an unquenchable thirst for knowledge.<br/>
             Right now I'm focused on React, Redux and Sass.
+
         </p>
+        <div className="weather">
+            <p>Temperature in Stockholm is: {currentTemp}<span>&#8451;</span></p>
         </div>
-    );
+    </div>
+);
 }
 export default SideNavHeader;
